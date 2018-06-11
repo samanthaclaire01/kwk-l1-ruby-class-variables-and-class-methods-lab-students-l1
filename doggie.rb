363bc355a64f9a6928c83cc268d386d03b7f390b
Samantha Cassity <cassityclaire@gmail.com>
2018-06-11 16:02:33 +0000

    automatically backed up by learn

---
+++ doggie.rb
@@ -0,0 +1,20 @@
+class Dog 
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+  def name
+    @this_dogs_name
+  end
+  def breed=(dog_breed)
+    @this_dogs_breed = breed
+  end 
+  def breed
+    @this_dogs_breed
+  end 
+  def bark
+    puts "Woof!"
+  end
+end
+buddy = Dog.new 
+buddy.name = "Buddy"
+puts "This dogs name is #{buddy.name}"
